9d0cf6b15747b353ae35a34911048bde36781d7a
Håvard Bakke <habakke@matrise.net>
2016-04-20 18:54:52 -0700

    Fixed problem by adding @Autowired

--- src/main/java/no/bluebit/demo/SecurityConfiguration.java
+++ src/main/java/no/bluebit/demo/SecurityConfiguration.java
@@ -1,35 +1,37 @@
-package no.bluebit.demo;
+	package no.bluebit.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+	import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+	import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+	import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+	import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private CustomAuthenticationProvider customAuthenticationProvider;
+	@Configuration
+	@EnableWebSecurity
+	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+	public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .authenticationProvider(this.customAuthenticationProvider);
+		@Autowired
+		private CustomAuthenticationProvider customAuthenticationProvider;
+
+		@Autowired
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+				.authenticationProvider(this.customAuthenticationProvider);
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.authorizeRequests()
+					.antMatchers("/api/users/login").permitAll()    // Permit access for all to login REST service
+					.antMatchers("/").permitAll()				    // Neccessary to permit access to default document
+				.anyRequest().authenticated().and()				    // All other requests require authentication
+				.httpBasic().and()
+				.logout().and()
+				.csrf().disable();
+		}
 	}
-
-    @Override
-	protected void configure(HttpSecurity http) throws Exception {
-        http
-			.authorizeRequests()
-				.antMatchers("/api/users/login").permitAll()    // Permit access for all to login REST service
-				.antMatchers("/").permitAll()				    // Neccessary to permit access to default document
-            .anyRequest().authenticated().and()				    // All other requests require authentication
-            .httpBasic().and()
-            .logout().and()
-			.csrf().disable();
-	}
-}
